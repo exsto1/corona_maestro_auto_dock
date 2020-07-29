c8270a9e94851aac9a7775bd0db4f719d57073ae
exsto1 <exsto1@gmail.com>
2020-07-29 10:40:43 +0200

    .

--- maestro_workflow.py
+++ maestro_workflow.py
@@ -57,15 +57,15 @@ if __name__ == "__main__":
         exit()
 
     files = os.listdir("./ligands")
-
+    directory=os.getcwd()
 
     output = open(args.output, "w")
-    output.write('entryimport "%s"\nentryimport "%s"\nshowpanel superimpose\n\n' %(args.crystal, input))
+    output.write('entryimport "%s"\nentryimport "%s"\nshowpanel superimpose\n\n' %(args.crystal, directory +"/" + input))
 
     for i in range(len(titles_file)):
-        output.write("entryincludeonly s_m_title *%s*\n" % '_'.join(titles_file[i].split('_')[:2]))
-        output.write("entryinclude s_m_title %s\n" % '_'.join(titles_file[i].split('_')[:1]))
-        output.write('propertysuperimposesetting  applytoentries=included\nsuperimpose  inplace=false\nsuperimposeset atom.ptype " CA "\nsuperimpose  inplace=true')
-        output.write('uperimposesmarts "%s"\n\n' % SMARTS_save[i])
+        output.write("entrywsincludeonly s_m_title *%s*\n" % '_'.join(titles_file[i].split('_')[:2]))
+        output.write("entrywsinclude s_m_title %s\n" % '_'.join(titles_file[i].split('_')[:1]))
+        output.write('propertysuperimposesetting  applytoentries=included\nsuperimpose  inplace=false\nsuperimposeset atom.ptype " CA "\nsuperimpose  inplace=true\n')
+        output.write('superimposesmarts "%s" \n\n' % SMARTS_save[i].rstrip())
 
     output.close()
